e3477757a5c35b62fb29d8e325bea4d3fe6c20d0
alexnorrell <norrell.alex@gmail.com>
2023-02-26 13:40:42 -0500

    working on classes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-2
+const inquirer = require('inquirer')
+const questions = [
+    {
+        type:'input', name:'name', message:'What is your name?'
+    },
+    {
+        type:'input', name:'ID', message:'What is your employe ID?' 
+    },
+    {
+        type:'input', name:'email', message:'What are your email adress?'
+    },    
+    {
+        type:'input', name:'officeNumber', message:'What are your office number?'
+    }
+]
+function init() {
+    inquirer.prompt(questions).then(function(answers){
+        console.log(answers)
+    })
+}
+
+        init()
